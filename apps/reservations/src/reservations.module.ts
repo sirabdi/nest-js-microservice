038c46dfa1827f96deb42d1e5e66aa57e994d480
raelvanz <abdisembada567@gmail.com>
2025-06-09 22:51:11 +0700

    Initialize API CRUD

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import { ReservationsRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationsRepository],
+})
+export class ReservationsModule {}
