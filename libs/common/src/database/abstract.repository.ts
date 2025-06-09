1f3bf41b1bb021e85f3d063f0e85a7cf396d5526
Abdi Sembada Amirullah <abdi@undercurrent.tech>
2025-06-09 19:05:14 +0700

    Initialize #1

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,25 @@
+import { Model, Types } from 'mongoose';
+import { Logger } from '@nestjs/common';
+import { AbstractDocument } from './abstract.schema';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { DatabaseModule } from '@app/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
